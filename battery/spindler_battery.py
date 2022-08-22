cc7dab8bad91d7b8199ae4c4873c929f86a31dec
Won Kim <103711389+wkim10@users.noreply.github.com>
2022-08-22 12:08:36 +0900

    Add files via upload

---
+++ battery/spindler_battery.py
@@ -0,0 +1,16 @@
+from abc import ABC
+from datetime import datetime
+
+from battery import Battery
+
+class SpindlerBattery(Battery, ABC):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < self.current_date:
+            return True
+        else:
+            return False
